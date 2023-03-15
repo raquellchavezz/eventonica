c7c25e51e6dd04e2a3b0a660ad9119eb52c126a1
raquellchavezz <raquel.chavez41313@gmail.com>
2023-03-15 08:32:01 -0700

    fixed the post request

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(cors()); //anytime use mddlewear wil do app.use
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// //creates an endpoint for the route `/`
+// // //creates an endpoint for the route `/`
 app.get("/", (req, res) => {
   //when server has a req sent to it it'll look for what url is being used
   res.json("Hello Techtonica Server for an app with Events"); //--> localhost:8080
@@ -34,50 +34,92 @@ app.get("/api/events", async (req, res) => {
     console.log(error);
     return res.status(400).json({ error });
   }
-
-  //creating routes:
-
-  //create a event (add a resource --> POST)
-  app.post("/api/events", async (req, res) => {
-    //TO - DO - At the end => save this event to the db
-    //INSERT INTO events (title, location, eventtime) VALUES ('Women in Tech Techtonica Panel', 'Overland Park Convention Center', '2023-04-21')
-    try {
-      //console.log(req.body) //tried to console log here to see if I could add an event via postman but didn't work
-      const newEvent = {
-        title: req.body.title,
-        location: req.body.location,
-        eventtime: req.body.eventtime,
-      };
-      const result = await db.query(
-        //assigning result to the query that will insert the new event we just created
-        "INSERT INTO events(title, location, eventtime) VALUES ($1, $2, $3) RETURNING *",
-        [newEvent.title, newEvent.location, newEvent.eventtime]
-      );
-      let response = result.rows[0]; //first row in the query (what we just added?)
-      console.log(response);
-      res.json(response);
-    } catch (e) {
-      console.log(error);
-      return res.status(400).json({ error });
-    }
-  });
-
-  // update a event
-
-  //delete a event
-
-  //hardcode the events response for testing reasons. This call has one more event that the real DB
-  // const events = [
-
-  //     {id: 1, title: 'Women in Tech Techtonica Panel', location: 'Overland Park Convention Center'},
-  //     {id: 2, title: 'Japanese Cultural Education', location: 'Seattle Convention Center'},
-  //     {id: 3, title: "Haven 90's Party Night Club", location: 'Hilton Hotel Kansas City'},
-  //     {id: 4, title: 'Comedy Night at the Station', location: 'SF Hilton Hotel'},
-  //     {id: 5, title: 'A Decadent Arts Experience', location: 'West Ridge Mall'},
-  //     {id: 6, title: 'Techtonica Classroom Course', location: 'Techtonica HQ'}
-  //   ];
-  // res.json(events);
 });
+
+//   //creating routes:
+
+//create a event (add a resource --> POST)
+app.post("/api/events", async (req, res) => {
+  //TO - DO - At the end => save this event to the db
+  //INSERT INTO events (title, location, eventtime) VALUES ('Women in Tech Techtonica Panel', 'Overland Park Convention Center', '2023-04-21')
+  try {
+    //console.log(req.body) //tried to console log here to see if I could add an event via postman but didn't work
+    const newEvent = {
+      title: req.body.title,
+      location: req.body.location,
+      eventtime: req.body.eventtime,
+    };
+    const result = await db.query(
+      //assigning result to the query that will insert the new event we just created
+      "INSERT INTO events(title, location, eventtime) VALUES ($1, $2, $3) RETURNING *",
+      [newEvent.title, newEvent.location, newEvent.eventtime]
+    );
+    let response = result.rows[0]; //first row in the query (what we just added?)
+    console.log(response);
+    res.json(response);
+  } catch (e) {
+    console.log(error);
+    return res.status(400).json({ error });
+  }
+});
+
+// update a event (PUT) - not working
+
+// app.put("/api/events/:id"),
+//   async (req, res) => {
+//     try {
+//       const { id } = req.params; //we want to get the id of the requested parameters which will be the event id
+//       const updateEvent = {
+//         title: req.body.title,
+//         location: req.body.location,
+//         eventtime: req.body.eventtime,
+//       };
+//       const result = await db.query(
+//         //assigning result to the query that will insert the new event we just created
+//         "UPDATE events(title, location, eventtime) VALUES ($1, $2, $3) WHERE id = $4",
+//         [updateEvent.title, updateEvent.location, updateEvent.eventtime]
+//       );
+//       let response = result.rows[0]; //first row in the query (what we just added?)
+//       console.log(response);
+//       res.json(response);
+//     } catch (e) {
+//       console.log(error);
+//       return res.status(400).json({ error });
+//     }
+//   };
+
+//delete a event
+// app.delete("/api/events/:eventID", cors(), async (req, res) => {
+//   let eventIdToDelete = req.params.eventID; // :bookID is like a varaible/placeholder for what is being requested
+//   console.log("eventIdToDelete", eventIdToDelete); //2 is the requested book which is being placed in URL in spot of book id but it doesn't exist which is why it is not found 404 error
+//   //Is the response
+
+//   for (let i = 0; i < events.length; i++) {
+//     // will use for loop to iterate through the books array
+//     //for every book n the array of object called books --> books.js
+//     // we want to check to see if the deletedBook request matches an isbn in books
+//     if (eventIdToDelete === events[i].id) {
+//       let deletedEventTitle = events[i].title;
+//       console.log(events.splice(i, 1)); // so here we are deleting the isbn that matches at postion i and just deleting that 1 element
+
+//       return res.json("You deleted: " + deletedEventTitle); //
+//     }
+//   } //response for match below:
+//   res.sendStatus(404);
+// });
+
+//hardcode the events response for testing reasons. This call has one more event that the real DB
+// const events = [
+
+//     {id: 1, title: 'Women in Tech Techtonica Panel', location: 'Overland Park Convention Center'},
+//     {id: 2, title: 'Japanese Cultural Education', location: 'Seattle Convention Center'},
+//     {id: 3, title: "Haven 90's Party Night Club", location: 'Hilton Hotel Kansas City'},
+//     {id: 4, title: 'Comedy Night at the Station', location: 'SF Hilton Hotel'},
+//     {id: 5, title: 'A Decadent Arts Experience', location: 'West Ridge Mall'},
+//     {id: 6, title: 'Techtonica Classroom Course', location: 'Techtonica HQ'}
+//   ];
+// res.json(events);
+// });
 
 app.listen(PORT, () =>
   console.log(`Hola! Server running on Port http://localhost:${PORT}`)
