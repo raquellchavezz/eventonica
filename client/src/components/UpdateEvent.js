67e0dec786dce76458b06b2ebcf31778cceb41b0
raquellchavezz <raquel.chavez41313@gmail.com>
2023-03-16 10:56:01 -0700

    adds UI for the update event

--- client/src/components/UpdateEvent.js
+++ client/src/components/UpdateEvent.js
@@ -4,13 +4,14 @@
 import { useState } from "react"; //need to handle information
 //need this form to send this information to do the post request so we can actually add it on the page
 //^ dont want this happening here though, we would do it in events.js since this is the place that is already doing a request
-const AddEvent = (props) => {
+//component passing the data props in here
+const UpdateEvent = (props) => {
   //here we are passing in the props, could also call this data because its any data passed to the component from its parent which is events.js
   //This is my state with the initial values empty
   //we want to have a state so we can add events
   //useState will be an object with the keys already bc we want to pass the keys
   // the value of the keys will be empty though bc at the beginning we dont know what the user wants to add
-  const [event, setEvent] = useState({
+  const [updateEvent, setUpdateEvent] = useState({
     //we use the useState hook to manage state and update UI elements accordingly.
     //curent state value is initally an object with keys all set to empty strings
     //state here used in line 49
@@ -23,15 +24,15 @@ const AddEvent = (props) => {
   //we need handlechange functions so the browser doesnt reload when user types something new in each time
   //
   const handleTitleChange = (e) => {
-    //e is NOT the event(which is our data)
+    //not the event(which is our data)
     e.preventDefault(); //in order to see this function we have to call it
-    let newTitle = e.target.value; //we get the e target which is the value of the newTitle coming from the input type text field
+    let updateTitle = e.target.value; //we get the e target which is the value of the newTitle coming from the input type text field
     //if we console.log(e.target) --> <input type="text" id="add-event-title" placeholder="The Title of your Event" required="" value="">
     //so to target the area where it says value above, we need to do e.target.value
     //e.target is an event property that refers to the element that triggered the event.
     console.log("this is the e.target", e.target); // --> <input type="text" id="add-event-title" placeholder="The Title of your Event" required="" value="hello">
-    console.log("this is newTitle", newTitle); //-> this is newTitle hello (this is whats in the console when we changed the e.target.value instead of e.target)
-    setEvent((event) => ({ ...event, title: newTitle })); //set as new information for the event, curly braces bc state is an object, its how we are able to access/add info to our state
+    console.log("this is the updated Title", updateTitle); //-> this is newTitle hello (this is whats in the console when we changed the e.target.value instead of e.target)
+    setUpdateEvent((updateEvent) => ({ ...updateEvent, title: updateTitle })); //set as new information for the event, curly braces bc state is an object, its how we are able to access/add info to our state
     //used the spread operator to copy all the events, and just change the title of the event we want to change
     //adding directly into the obj, we just want to change the title
     //spreaad operator bc we dont want to lose all the information when changing only the title, we want to keep location, daate, etc
@@ -39,19 +40,25 @@ const AddEvent = (props) => {
     //setEvent is a function which recieves the original information from event
     //then an anon func will be passed the events with the spread operator and the new thing we want to change
     //new value of title is newTitle which is the e.target.value which targets the
-    console.log(event.title); //this is making sure we are console.logging what the user enters for the title
+    console.log(updateEvent.title); //this is making sure we are console.logging what the user enters for the title
   };
   const handleLocationChange = (e) => {
     // handle changes to a location input.
     e.preventDefault();
-    let newLocation = e.target.value; //targets new location user inputs
-    setEvent((event) => ({ ...event, location: newLocation }));
+    let updateLocation = e.target.value; //targets the update of location which user inputs
+    setUpdateEvent((UpdateEvent) => ({
+      ...updateEvent,
+      location: updateLocation,
+    }));
     //console.log(event.location);
   };
   const handleDateChange = (e) => {
     e.preventDefault();
-    let newDate = e.target.value;
-    setEvent((event) => ({ ...event, eventtime: newDate }));
+    let updateDate = e.target.value;
+    setUpdateEvent((updateEvent) => ({
+      ...updateEvent,
+      eventtime: updateDate,
+    }));
     //console.log(event.eventtime);
   };
 
@@ -59,11 +66,11 @@ const AddEvent = (props) => {
     //e = event of the form
     //handles the submit so not grabbing every single character the user types
     e.preventDefault(); //stop browser reloading bc havent connected to server yet
-    setEvent(event); //here is where you would set the whole event
+    setUpdateEvent(updateEvent); //here is where you would set the whole event
     //when setting event to event, stored each piece in event and at once updating overall state
     //SetEvent sets final updated event
-    console.log("this is from handleSubmit", event);
-    props.postRequest(event); //passing the postRequest here
+    console.log("this is from handleSubmit", updateEvent);
+    props.putRequest(UpdateEvent); //passing the postRequest here
     //data.postRequest(event)
   };
 
@@ -80,7 +87,7 @@ const AddEvent = (props) => {
         id="add-event-title" //field 1
         placeholder="The Title of your Event"
         required
-        value={event.title} //need state
+        value={updateEvent.title} //need state
         onChange={handleTitleChange} //onChange, when user changes something in the title, we will use the handleTitleChange fucntion
         //onChange is very particular to this input, only has access to the value inside --> value {event.title}
         //onChange updates property in inital state
@@ -91,20 +98,20 @@ const AddEvent = (props) => {
         id="add-event-location"
         placeholder="The Location of your Event"
         required
-        value={event.location} //state
+        value={updateEvent.location} //state
         onChange={handleLocationChange}
       />
       <label>Date</label> {/*adding another input for dat, field 3*/}
       <input
         type="date"
         id="add-event-date"
-        value={event.eventtime} //state
+        value={updateEvent.eventtime} //state
         onChange={handleDateChange}
       />
-      <button type="submit">Add Event</button>{" "}
+      <button type="submit">UpdateEvent</button>{" "}
       {/*if have handleSubmit then we need a button so user can actually submit */}
     </form>
   );
 };
 
-export default AddEvent;
+export default UpdateEvent;
--- client/src/components/events.js
+++ client/src/components/events.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import EventCard from "./eventCard";
 import CardGroup from "react-bootstrap/CardGroup";
 import AddEvent from "./AddEvent";
+import UpdateEvent from "./UpdateEvent"; //this is a child of events.js
 //we want to pass the AddEvent component to events.js so we can use the handle change functions thru out all the events
 // similar to get request on event, we want to do post request on event bc if have new data we want
 // that events to update itself and update the new event
@@ -9,19 +10,62 @@ import AddEvent from "./AddEvent";
 //so we need to use a function to do so
 //1. start clean new react app see how this behaves
 //2. edit to do less
+//template for all the events pass data back and forth from events.js
+
 function Events() {
-  const [events, setEvents] = useState([]);
-  console.log("Events start render of state events", events); //happening each time events i sbeing rendered
+  const [events, setEvents] = useState([]); //setting intial valye of events as an empty array
+  console.log("Events initial render of events, empty array", events); //happening each time events i sbeing rendered
+  //we want to fetch data from the url below
+  //once successfully fetch data will update using state using setEvents with the data we get back
+
+  //fetch is used to send HTTP requests to the backend to retrieve data or send data for processing.
   useEffect(() => {
-    fetch("http://localhost:8080/api/events") //default method is get , specifying url fetching frm
-      .then((response) => response.json())
+    //cant use async here using the useEffect hook which will run at the minute the component first renders & only time will run bc has empty dependency array inside of it
+    //fetch this data ONCE when this component first renders
+    //this is a fetch request to the backend
+    fetch("http://localhost:8080/api/events") //default method is get—so this here is a get request , specifying url fetching frm this is our endpoint
+      //get req = retrieve and return a specific resource (usually a web page or data) from a specified URL.
+      //this returns to us a promise
+      //HTTP request to the backend, specifying the endpoint and any necessary
+      // data in the request body or query parameters -->  two ways to send additional data to the server.
+      //.then will fire a function once the promise is resolved and we have the data back
+      .then((response) => response.json()) //this response obj isnt actually the data, its just a response object
+      //in order to get the data need to do something with the response object which is when we use the fetch api
+      //to get the data we do response.json --> passes the data into JSON into a javascript object for us
       .then((responseEvents) => {
-        setEvents(responseEvents);
+        //refers to the parsed JSON data after first .then
         console.log("Events fetched...", responseEvents);
+        //responseEvents is the actual data
+        //fires this function once the above .then is complete
+        //^ this takes in(as a param)the actual data that response.json gets us
+        setEvents(responseEvents); //updates the state of the component with the fetched data
+        // so that the UI can be re-rendered with the new data.
+
         console.log("events after the fetch", events);
       });
   }, []); //whenever array changes will run useEffect, controls when component rerenders
+  //empty array =  indicates that the effect should only be executed once, when the component is mounted/loads.
 
+  //post request on frontend
+
+  //function postRequest(newEvent){
+  //   return fetch("http://localhost:8080/api/events", {
+  //     //
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" }, //when req made from web browser to backend server, req has multiple parts
+  //     //header has extra info
+  //     body: JSON.stringify(newEvent),
+  //   })
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       //console.log("From the front", data);
+  //       setEvents((events) => [...events, data]);
+  //     });
+  // };
+
+  //}
   const postRequest = (newEvent) => {
     //need to use this in the child, so need to pass as props
     //console.log("From the parent", newEvent);
@@ -41,6 +85,28 @@ function Events() {
       });
   };
 
+  //put request
+  const putRequest = (eventId, updatedEvent) => {
+    //need to use this in the child, so need to pass as props
+    //console.log("From the parent", newEvent);
+    return fetch("http://localhost:8080/api/${eventId}", {
+      //
+      method: "PUT",
+      headers: { "Content-Type": "application/json" }, //when req made from web browser to backend server, req has multiple parts
+      //header has extra info
+      body: JSON.stringify(updatedEvent), //updatedEvent is an object containing the updated data for the event now transformed to JSON format
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        //console.log("From the front", data);
+        setEvents((events) =>
+          events.map((event) => (event.id === data.id ? data : event))
+        );
+      });
+  };
+
   return (
     <div>
       <CardGroup className="Events">
@@ -55,7 +121,15 @@ function Events() {
           ) //passing props here
         )}
       </CardGroup>
-      <AddEvent postRequest={postRequest} /> {/*passing the prop but why?*/}
+      <AddEvent postRequest={postRequest} />
+
+      <UpdateEvent putRequest={putRequest} />
+      {/*passing the prop to the AddEvent component so it will have access to the prop postRequest with a value of postRequest*/}
+      {/*this allows the component to get the data/functions from its parent component*/}
+      {/*, the AddEvent component will have access to the postRequest
+ function as a prop, which it can then use to make a request to the server or 
+ update the state of the parent component.*/}
+      {/*dont want to render addEvent until we have an output */}
     </div>
   );
 }
